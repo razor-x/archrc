0ff203ccee2d4c14005505bf1b99037d0c368522
Evan Sosenko <razorx@evansosenko.com>
2025-11-14 21:35:57 -0800

    Add a sudoers file

--- manifest.js
+++ manifest.js
@@ -1,1 +1,25 @@
-export default {}
+const ioType = 'linux'
+const pkgType = 'pacman'
+
+const targetRoot = '/'
+
+const defaults = {
+  dmode: '0755',
+  fmode: '0644',
+  user: 'root',
+  group: 'root'
+}
+
+const files = [{
+  src: 'etc/sudoers',
+  fmode: '0440',
+  pkgs: ['sudo']
+}]
+
+export default {
+  files,
+  targetRoot,
+  ioType,
+  pkgType,
+  defaults
+}
