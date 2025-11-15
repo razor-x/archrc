5803799930517ae4a752450219eb88693a0b1e56
Evan Sosenko <razorx@evansosenko.com>
2025-11-14 20:02:48 -0800

    Add basic packages for aconfmgr

---
+++ aconfmgr/20-system.sh
@@ -0,0 +1,22 @@
+# Platform
+
+AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage base-devel # Basic tools to build Arch Linux packages
+AddPackage linux # The Linux kernel and modules
+AddPackage linux-firmware # Firmware files for Linux - Default set
+AddPackage efivar # Tools and libraries to work with EFI variables
+AddPackage sudo # Give certain users the ability to run some commands as root
+
+if [ "$is_virtualbox" = true ]; then
+	AddPackage virtualbox-guest-utils # VirtualBox Guest userspace utilities
+fi
+
+# Configuration
+
+AddPackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign aura # A package manager for Arch Linux and its AUR
+AddPackage --foreign aura-debug # Detached debugging symbols for aura
+
+# Services
+AddPackage openssh # SSH protocol implementation for remote login, command execution and file transfer
---
+++ aconfmgr/20-user.sh
@@ -0,0 +1,19 @@
+# Core
+
+AddPackage git # the fast distributed version control system
+AddPackage rsync # A fast and versatile file copying tool for remote and local files
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+
+# Development
+
+## Rust
+AddPackage rust # Systems programming language focused on safety, speed and concurrency
+
+## Node.js
+AddPackage nodejs # Evented I/O for V8 javascript ("Current" release)
+AddPackage npm # JavaScript package manager
+
+# GUI
+AddPackage sway # Tiling Wayland compositor and replacement for the i3 window manager
+AddPackage foot # Fast, lightweight, and minimalistic Wayland terminal emulator
+AddPackage wmenu # Efficient dynamic menu for Wayland and wlroots based Wayland compositors
