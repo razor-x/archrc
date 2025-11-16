971b7524a873a979d83d6e3ed7b2ed6c7cd1a2f9
Evan Sosenko <razorx@evansosenko.com>
2025-11-15 20:41:16 -0800

    Run systemctl with sudo

--- aconfmgr/10-10-systemd-helpers.sh
+++ aconfmgr/10-10-systemd-helpers.sh
@@ -9,7 +9,8 @@ function SystemdEnable() {
   [[ $# -ne 1 ]] && FatalError "Expected 1 arguments, got $#."
   local unit=$1
   mkdir -p "$tmp_dir"/systemd
-  if systemctl status "$unit" >/dev/null 2>&1; then
+  touch "$tmp_dir/systemd/units"
+  if sudo -S systemctl status "$unit" >/dev/null 2>&1; then
     printf "%s\n" "$unit" >> "$tmp_dir/systemd/units"
   fi
 }
