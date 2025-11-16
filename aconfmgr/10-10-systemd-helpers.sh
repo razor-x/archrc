b9686e6df3661f8b6c54a6b7fe7573e1783a9191
Evan Sosenko <razorx@evansosenko.com>
2025-11-15 20:38:07 -0800

    Only install unit if exists

--- aconfmgr/10-10-systemd-helpers.sh
+++ aconfmgr/10-10-systemd-helpers.sh
@@ -9,5 +9,7 @@ function SystemdEnable() {
   [[ $# -ne 1 ]] && FatalError "Expected 1 arguments, got $#."
   local unit=$1
   mkdir -p "$tmp_dir"/systemd
-  printf "%s\n" "$unit" >> "$tmp_dir/systemd/units"
+  if systemctl status "$unit" >/dev/null 2>&1; then
+    printf "%s\n" "$unit" >> "$tmp_dir/systemd/units"
+  fi
 }
