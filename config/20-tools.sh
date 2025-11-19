b5b4322090262fea0155f8e36e10e4722c61a28a
Evan Sosenko <razorx@evansosenko.com>
2025-11-19 00:04:22 -0800

    Add core tools (uncategorized)

--- config/20-tools.sh
+++ config/20-tools.sh
@@ -3,3 +3,32 @@ AddPackage less # A terminal based program for viewing text files
 AddPackage man-db # A utility for reading man pages
 AddPackage efivar # Tools and libraries to work with EFI variables
 AddPackage dnssec-tools # libval & dnssec management tools
+
+AddPackage blas-openblas # An optimized BLAS library based on GotoBLAS2 1.13 BSD (Provides BLAS/CBLAS/LAPACK/LAPACKE system-wide)
+AddPackage cabal-install # The command-line interface for Cabal and Hackage.
+AddPackage composer # Dependency Manager for PHP
+AddPackage ctags # Generates an index file of language objects found in source files
+AddPackage gcc-fortran # Fortran front-end for GCC
+AddPackage ghc # The Glasgow Haskell Compiler
+AddPackage git-lfs # Git extension for versioning large files
+AddPackage github-cli # The GitHub CLI
+AddPackage go # Core compiler tools for the Go programming language
+AddPackage jdk-openjdk # OpenJDK Java 25 development kit
+AddPackage leiningen # Automate Clojure projects
+AddPackage libgit2 # A linkable library for Git
+AddPackage libyaml # YAML 1.1 library
+AddPackage linux-headers # Headers and scripts for building modules for the Linux kernel
+AddPackage perl-libwww # The World-Wide Web library for Perl
+AddPackage php # A general-purpose scripting language that is especially suited to web development
+AddPackage r # Language and environment for statistical computing and graphics
+AddPackage zsh # A very advanced and programmable command interpreter (shell) for UNIX
+
+
+AddPackage 7zip # File archiver for extremely high compression
+AddPackage jq # Command-line JSON processor
+AddPackage moreutils # A growing collection of the unix tools that nobody thought to write thirty years ago
+AddPackage openbsd-netcat # TCP/IP swiss army knife. OpenBSD variant.
+AddPackage rclone # rsync for cloud storage
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage unrar-free # Free utility to extract files from RAR archives
+AddPackage wget # Network utility to retrieve files from the web
