3a217ccfc645af799c11af6e35d1bb3a223fd565
Evan Sosenko <razorx@evansosenko.com>
2025-11-27 00:24:14 -0800

    Add unsorted packages

---
+++ config/99-unsorted.sh
@@ -0,0 +1,25 @@
+
+
+# Thu Nov 27 12:14:18 AM PST 2025 - Unknown packages
+
+
+AddPackage bat-extras # Bash scripts that integrate bat with various command line tools
+AddPackage python-matplotlib # A python plotting library, making publication quality plots
+AddPackage python-scipy # Open-source software for mathematics, science, and engineering
+
+
+# Thu Nov 27 12:24:05 AM PST 2025 - Unknown packages
+
+
+AddPackage chafa # Image-to-text converter supporting a wide range of symbols and palettes, transparency, animations, etc.
+AddPackage imagemagick # An image viewing/manipulation program
+AddPackage poppler # PDF rendering library based on xpdf 3.0
+AddPackage prettier # An opinionated code formatter
+AddPackage python-black # Uncompromising Python code formatter
+AddPackage python-jedi # Awesome autocompletion for python
+AddPackage python-watchdog # Python API and shell utilities to monitor file system events
+AddPackage resvg # SVG rendering library and CLI
+AddPackage rlwrap # Adds readline-style editing and history to programs
+AddPackage ruby-docs # Documentation files for Ruby
+AddPackage shfmt # Format shell programs
+AddPackage usage # A specification for CLIs
