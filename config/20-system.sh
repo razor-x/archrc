871439272ccf44e922ed0f053a976836cb57fee9
Evan Sosenko <razorx@evansosenko.com>
2025-12-28 11:24:03 -0800

    Set sudoers.d modes

--- config/20-system.sh
+++ config/20-system.sh
@@ -77,8 +77,8 @@ sed -i \
 
 ## sudo
 AddPackage sudo # Give certain users the ability to run some commands as root
-CopyFile /etc/sudoers.d/01_root
-CopyFile /etc/sudoers.d/02_commands
+CopyFile /etc/sudoers.d/01_root 400
+CopyFile /etc/sudoers.d/02_commands 400
 
 ## PAM
 CopyFile /etc/security/faillock.conf
