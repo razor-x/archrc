02d753eef4687db91612cf0ab63b5ba4be2ab49e
Evan Sosenko <razorx@evansosenko.com>
2025-12-28 00:38:34 -0800

    Add cmake

--- config/40-devel.sh
+++ config/40-devel.sh
@@ -10,6 +10,7 @@ AddPackage sqlite # A C library that implements an SQL database engine
 # Languages
 
 ## C
+AddPackage cmake # A cross-platform open-source make system
 AddPackage gcc # The GNU Compiler Collection - C and C++ frontends
 AddPackage gcc-fortran # Fortran front-end for GCC
 AddPackage clang # C language family frontend for LLVM
