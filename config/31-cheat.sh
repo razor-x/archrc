57f79bfd99e06e3a5824e5f552e8a7d0cc2be58e
Evan Sosenko <razorx@evansosenko.com>
2025-12-28 00:18:33 -0800

    Add rlwrap

---
+++ config/31-cheat.sh
@@ -0,0 +1,3 @@
+# UPSTREAM: cht.sh is installed in user space but requires rlwrap.
+# https://github.com/chubin/cheat.sh#installation
+AddPackage rlwrap # Adds readline-style editing and history to programs
