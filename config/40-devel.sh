1b2a76fa0639ce04c54d43070e9eece90a88ac5f
Evan Sosenko <razorx@evansosenko.com>
2026-01-04 11:26:41 -0800

    Move unsorted tools to devel

--- config/40-devel.sh
+++ config/40-devel.sh
@@ -88,13 +88,15 @@ AddPackage uv # An extremely fast Python package installer and resolver written 
 
 ## Ruby
 AddPackage ruby # An object-oriented language for quick and easy programming
+AddPackage ruby-docs # Documentation files for Ruby
 AddPackage ruby-lsp # An opinionated language server for Ruby
 AddPackage ruby-stdlib # Full Ruby StdLib including default gems, bundled gems and tools
 AddPackage ruby-pry # A runtime developer console and IRB alternative with powerful introspection capabilities
 
 ## Shell
-AddPackage shellcheck # Shell script analysis tool
 AddPackage bash-language-server # Bash language server implementation based on Tree Sitter and its grammar for Bash
+AddPackage shellcheck # Shell script analysis tool
+AddPackage shfmt # Format shell programs
 
 ## Web
 AddPackage vscode-css-languageserver # CSS/LESS/SCSS language server
