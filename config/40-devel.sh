86844a59dccf19adcec74c32fe4cb519c780239c
Evan Sosenko <razorx@evansosenko.com>
2026-01-03 16:00:10 -0800

    Add more devel packages

--- config/40-devel.sh
+++ config/40-devel.sh
@@ -15,6 +15,9 @@ AddPackage libffi # Portable foreign function interface library
 AddPackage libgit2 # A linkable library for Git
 AddPackage libyaml # YAML 1.1 library
 AddPackage perl-libwww # The World-Wide Web library for Perl
+AddPackage python-matplotlib # A python plotting library, making publication quality plots
+AddPackage python-numpy # Scientific tools for Python
+AddPackage python-pandas # High-performance, easy-to-use data structures and data analysis tools for Python
 AddPackage sqlite # A C library that implements an SQL database engine
 
 ## Native Modules
@@ -22,6 +25,15 @@ AddPackage cmake # A cross-platform open-source make system
 AddPackage gcc-fortran # Fortran front-end for GCC
 
 # Languages
+#
+# Included for each language where possible:
+# - Compiler or interpreter
+# - Language Server Protocol (LSP)
+# - Standard libraries
+# - Package/project/dependency manager
+# - Formatter
+# - Linter
+# - REPL
 
 ## C/C++
 AddPackage clang # C language family frontend for LLVM
@@ -29,6 +41,7 @@ AddPackage clang # C language family frontend for LLVM
 ## Clojure
 AddPackage clojure # Lisp dialect for the JVM
 AddPackage leiningen # Automate Clojure projects
+AddPackage --foreign clojure-lsp-bin # Language Server (LSP) for Clojure
 
 ## Go
 AddPackage go # Core compiler tools for the Go programming language
@@ -43,12 +56,14 @@ AddPackage cabal-install # The command-line interface for Cabal and Hackage.
 ## Lua
 AddPackage lua # Powerful lightweight programming language designed for extending applications
 AddPackage lua-sec # Lua bindings for OpenSSL library to provide TLS/SSL communication for Lua 5.4
+AddPackage lua-language-server # Lua Language Server coded by Lua
 AddPackage stylua # Deterministic code formatter for Lua
 AddPackage selene # Blazing-fast modern Lua linter written in Rust
 AddPackage luarocks # Deployment and management system for Lua modules
 
-## Node.js
+## JavaScript/TypeScript
 AddPackage nodejs # Evented I/O for V8 javascript ("Current" release)
+AddPackage typescript-language-server # Language Server Protocol (LSP) implementation for TypeScript using tsserver
 AddPackage npm # JavaScript package manager
 AddPackage pnpm # Fast, disk space efficient package manager
 AddPackage yarn # Fast, reliable, and secure dependency management
@@ -59,7 +74,7 @@ AddPackage composer # Dependency Manager for PHP
 
 ## Python
 AddPackage python # The Python programming language
-AddPackage python-numpy # Scientific tools for Python
+AddPackage python-lsp-server # Fork of the python-language-server project, maintained by the Spyder IDE team and the community
 AddPackage bpython # Fancy ncurses interface to the Python interpreter
 AddPackage ruff # An extremely fast Python linter, written in Rust
 AddPackage python-pip # The PyPA recommended tool for installing Python packages
@@ -68,14 +83,15 @@ AddPackage uv # An extremely fast Python package installer and resolver written 
 
 ## Ruby
 AddPackage ruby # An object-oriented language for quick and easy programming
+AddPackage ruby-lsp # An opinionated language server for Ruby
 AddPackage ruby-stdlib # Full Ruby StdLib including default gems, bundled gems and tools
 AddPackage ruby-pry # A runtime developer console and IRB alternative with powerful introspection capabilities
 
-## R
-AddPackage r # Language and environment for statistical computing and graphics
-
-## Rust
-AddPackage rust # Systems programming language focused on safety, speed and concurrency
-
 ## Shell
 AddPackage shellcheck # Shell script analysis tool
+AddPackage bash-language-server # Bash language server implementation based on Tree Sitter and its grammar for Bash
+
+## Web
+AddPackage vscode-css-languageserver # CSS/LESS/SCSS language server
+AddPackage vscode-html-languageserver # HTML language server
+AddPackage vscode-json-languageserver # JSON language server
